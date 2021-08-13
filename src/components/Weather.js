5378ff27e8262b82b4ca651c0472b992774d11ad
Jessa Mae Alcantara <the.epikskeptic@gmail.com>
2021-08-13 00:23:50 -0400

    Updated Weather.js fetch to have loading function

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,14 +1,14 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState} from 'react'
 
 const Weather = () => {
     const [lat, setLat] = useState([])
     const [long, setLong] = useState([])
-    const [data, setData] = useState({
-        "main": {'temp': 0},
-        "weather": [{"main":'Weather'}],
-    })
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true)
+    
 
-    useEffect(() => {
+     useEffect(() => {
+
         const fetchData = async () => {
             navigator.geolocation.getCurrentPosition(function(position) {
                 setLat(position.coords.latitude);
@@ -18,18 +18,25 @@ const Weather = () => {
             await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
             .then(res => res.json())
             .then(result => {
-                setData(result);
+                if (result.cod === 200) {
+                    setData(result);
+                    setLoading(false);
+                }
                 console.log(result);
-            });
+            })
+            .catch ((err) => console.log(err));
         }
-        fetchData();
-    }, [lat,long, data]);
+            fetchData();
+
+        
+    }, [lat,long,loading]);
+
     return (
         <div className='weather-container'>
             <h1 className='weather-title'>Weather Forecast</h1>
-            <h2 className="temp">{data.main['temp']} &deg;C</h2>
-            <h2 className="description">{data.weather[0]['main']}</h2>
-            <h3 className="city">{data.name}</h3>
+            {loading ? <h2>Loading...</h2> : <h2 className="temp">{data.main['temp']} &deg;C</h2>}
+            {loading ? <h2>Loading...</h2> : <h2 className="description">{data.weather[0].main}</h2>}
+            {loading ? <h2>Loading...</h2> : <h3 className="city">{data.name}</h3>}
             
         </div>
     )
